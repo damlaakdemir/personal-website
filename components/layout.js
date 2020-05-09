11931b455b85f8d36088340057645cedd9f8ce99
Damla Akdemir <damlaakdemr@gmail.com>
2020-05-09 15:12:06 +0300

    create layout for personal website

---
+++ components/layout.js
@@ -0,0 +1,21 @@
+import Head from 'next/head';
+import React from 'react';
+import Navigation from './navigation'
+import Main from './main'
+
+function Layout({ children }) {
+    return (
+        <div className="row">
+            <Head>
+                <title>Damla Akdemir | Front End Developer</title>
+                <meta http-equiv="x-ua-compatible" content="ie=edge"/>
+                <meta charSet="utf-8"/>
+                <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+            </Head>
+            <Navigation />
+            <main className="col-lg-8 col-md-8 col-sm-12 col-12">{children}</main>
+        </div>
+    )
+}
+
+export default Layout
---
+++ pages/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Layout from '../components/layout'
+
+function HomePage() {
+    return <Layout>
+        dfdfs
+    </Layout>
+
+}
+
+export default HomePage
