ce7df036618e242ba6a1f036ac83c27c99e9117b
Damla Akdemir <damlaakdemr@gmail.com>
2020-05-10 18:27:01 +0300

    added blog.js, fontawesome imported and refactoring scss

---
+++ pages/blog.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import Link from 'next/link';
+import Layout from '../components/layout'
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCalendarAlt, faTags } from '@fortawesome/free-solid-svg-icons'
+
+function Blog() {
+    return <Layout>
+        <div className="main">
+            <h3 className="main__title">Blog</h3>
+            <Link href="//medium.com/@damlaakdemr/local-storage-ve-session-storage-nedir-nas%C4%B1l-kullan%C4%B1l%C4%B1r-237aa647b412">
+                <a target="_blank">
+                    <div className="blog__summary mt-5">
+                        <h5>Local Storage ve Session Storage Nedir? Nasıl Kullanılır?</h5>
+                        <span className="blog__summary-info"><FontAwesomeIcon icon={ faCalendarAlt } /> 17 Kasım 2019</span>
+                        <span className="blog__summary-info"><FontAwesomeIcon icon={ faTags } /> Session Storage, Local Storage, Web Storage, Cookies, Javascript</span>
+                    </div>
+                </a>
+            </Link>
+            <Link href="//medium.com/@damlaakdemr/angular-ile-routing-i%CC%87%C5%9Flemleri-c9c4a2826b27">
+                <a target="_blank">
+                    <div className="blog__summary mt-5">
+                        <h5>Angular ile Routing İşlemleri</h5>
+                        <span className="blog__summary-info"><FontAwesomeIcon icon={ faCalendarAlt } /> 22 Aralık 2019</span>
+                        <span className="blog__summary-info"><FontAwesomeIcon icon={ faTags } /> Angular, Routing, Component, Router, Angular Routing</span>
+                    </div>
+                </a>
+            </Link>
+        </div>
+    </Layout>
+}
+
+export default Blog
--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,46 @@ import Layout from '../components/layout'
 
 function HomePage() {
     return <Layout>
-        dfdfs
+        <div className="main">
+            <h3 className="main__title">Hakkımda</h3>
+            <div>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Assumenda culpa, deserunt inventore ipsam
+                itaque minima omnis perspiciatis reiciendis. Asperiores consequuntur earum error, hic laborum officiis
+                voluptates! Assumenda natus perspiciatis voluptas?
+            </div>
+            <div>A fugit quaerat quia vitae! Accusamus assumenda blanditiis corporis debitis, et fuga magni maiores
+                molestiae neque, quas quos saepe. Atque cupiditate distinctio enim ex excepturi quisquam sapiente sequi
+                unde, veritatis!
+            </div>
+            <div>Accusantium aliquam aspernatur atque consequatur cupiditate delectus dolores earum exercitationem
+                facilis in ipsam iusto magni, minus nisi obcaecati porro quidem recusandae reiciendis repudiandae sed
+                sint soluta sunt ullam, velit voluptatem!
+            </div>
+            <div>Consectetur dolorum ea eius iste maiores nulla officiis, quod reiciendis totam vel? Asperiores dolorem
+                eum pariatur perspiciatis ullam! Architecto assumenda aut consectetur culpa deleniti error ipsam nemo
+                nobis quis quod?
+            </div>
+            <div>A ab accusamus animi architecto aspernatur consequatur culpa ducimus earum, enim error esse impedit,
+                incidunt itaque iusto molestiae nisi officiis perferendis, placeat porro praesentium quibusdam quidem
+                quis ratione reiciendis ut.
+            </div>
+            <h3 className="main__title mt-5">Deneyimler</h3>
+            <h5 className="mt-3">Aweb Yazılım & Danışmanlık</h5>
+            <div>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Assumenda culpa, deserunt inventore ipsam
+                itaque minima omnis perspiciatis reiciendis. Asperiores consequuntur earum error, hic laborum officiis
+                voluptates! Assumenda natus perspiciatis voluptas?
+            </div>
+            <h5 className="mt-3">Lookus Scientific</h5>
+            <div>A fugit quaerat quia vitae! Accusamus assumenda blanditiis corporis debitis, et fuga magni maiores
+                molestiae neque, quas quos saepe. Atque cupiditate distinctio enim ex excepturi quisquam sapiente sequi
+                unde, veritatis!
+            </div>
+            <h5 className="mt-3">Testinium</h5>
+            <div>Accusantium aliquam aspernatur atque consequatur cupiditate delectus dolores earum exercitationem
+                facilis in ipsam iusto magni, minus nisi obcaecati porro quidem recusandae reiciendis repudiandae sed
+                sint soluta sunt ullam, velit voluptatem!
+            </div>
+        </div>
+
     </Layout>
 
 }
